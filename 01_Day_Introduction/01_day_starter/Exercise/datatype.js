8b87cbe9bf024537d831521560c359e6faa77806
André <andre.kreutzer@outlook.com>
2023-01-23 13:03:48 -0300

    Exercise day 01

---
+++ 01_Day_Introduction/01_day_starter/Exercise/datatype.js
@@ -0,0 +1,10 @@
+// console.log(
+//   typeof helloWorld,
+//   typeof welcome,
+//   typeof imLearning,
+//   typeof undefined2,
+//   typeof value,
+//   typeof withourValue1,
+//   typeof withourValue2,
+//   typeof withourValue3
+// );
---
+++ 01_Day_Introduction/01_day_starter/Exercise/variable.js
@@ -0,0 +1,14 @@
+let firstName = "André Luiz",
+  lastName = "Kreutzer de Jesus",
+  country = "Brasil",
+  age = "25";
+
+const isMarried = false;
+
+console.log(firstName, lastName, isMarried, country, age);
+
+let myAge = "I am 25 years old.",
+  yourAge = "You are 30 years old.";
+
+console.log(myAge);
+console.log(yourAge);
